8bc9b950f58681de0220e6d587e2f7c90747cbb5
Charbel Breydy Torres <66335475+Buly1601@users.noreply.github.com>
2025-11-10 14:38:27 -0600

    Update dashboard.py

--- OpenSource_2025/dashboard.py
+++ OpenSource_2025/dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import paho.mqtt.client as mqtt
 from collections import deque
 import time
-"webhook?"
+
 BROKER = "broker.hivemq.com"
 TOPIC = "iot/room/temperature"
 
@@ -27,4 +27,5 @@ while True:
     if temps:
         placeholder.line_chart(list(temps))
     time.sleep(1)
+
 
